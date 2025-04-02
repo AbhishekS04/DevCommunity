e2283bd1739e27c7028ff3c0ea358c64fd4ec042
AbhishekS04 <abhishek23main@gmail.com>
2025-04-02 10:00:02 +0530

    initialize project structure with essential configurations and components

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,55 @@
+import Button from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
+
+export default function Hero() {
+    return (
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-32 top-16 hidden lg:block">
+                    <Image src={designExample1Image} alt="Design Example 1" />
+                </div>
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image src={designExample2Image} alt="Design Example 2" />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Test1" color="blue" />
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="Test2" color="red" />
+                </div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨ 10+ active members
+                    </div>
+                </div>
+                <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
+                    Code Together, Grow Together
+                </h1>
+                <p className="text-center text-lg sm:text-xl text-white/50 mt-8 max-w-2xl mx-auto px-4">
+                    Join a vibrant community of developers. Share knowledge,
+                    build projects, and accelerate your growth.
+                </p>
+                <div className="px-4 sm:px-0">
+                    <form className="flex flex-col sm:flex-row gap-2 sm:gap-0 border border-white/15 rounded-full p-2 mt-8 max-w-md mx-auto">
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            className="bg-transparent px-4 flex-1 w-full"
+                        />
+                        <Button
+                            type="submit"
+                            variant="primary"
+                            className="whitespace-nowrap"
+                            size="sm"
+                        >
+                            Sign up
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ src/sections/Introduction.tsx
@@ -0,0 +1,20 @@
+import Tag from "@/components/Tag";
+
+const text = ` Building a developer community where innovation thrives, free from complexity and barriers, empowering creators to collaborate and grow.`;
+
+export default function Introduction() {
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                <Tag>Introducing DevX</Tag>
+                </div>
+                <div className="text-4xl text-center font-medium mt-10">
+                    <span>Your creativity deserves a better community</span>
+                    <span className="text-white/15">{text}</span>
+                    <span className="text-lime-400"> That&apos;s why we created DevX</span>
+                </div>
+            </div>
+        </section>
+    );
+}
