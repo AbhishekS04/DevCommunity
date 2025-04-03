09bfbab9171c044b20b8bb07a3dc76cda4a874c5
AbhishekS04 <abhishek23main@gmail.com>
2025-04-03 22:52:05 +0530

    update Introduction section styles for improved layout and responsiveness

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -4,13 +4,13 @@ const text = ` Building a developer community where innovation thrives, free fro
 
 export default function Introduction() {
     return (
-        <section className="py-24">
+        <section className="py-28 lg:py-40">
             <div className="container">
                 <div className="flex justify-center">
                 <Tag>Introducing DevX</Tag>
                 </div>
-                <div className="text-4xl text-center font-medium mt-10">
-                    <span>Your creativity deserves a better community</span>
+                <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
+                    <span>Your creativity deserves a better community</span> {" "}
                     <span className="text-white/15">{text}</span>
                     <span className="text-lime-400"> That&apos;s why we created DevX</span>
                 </div>
